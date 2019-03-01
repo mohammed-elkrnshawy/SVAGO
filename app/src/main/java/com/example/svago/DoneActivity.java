0bc798c31e1cdc829ab4e7f2a0b3aa0a7fc1ed49
mohammed-elkrnshawy <mohammedelkrnshawy@gmail.com>
2019-03-01 23:53:15 +0200

    Review A

---
+++ app/src/main/java/com/example/svago/DoneActivity.java
@@ -0,0 +1,13 @@
+package com.example.svago;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DoneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_done);
+    }
+}

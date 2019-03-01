0aa8a49453935a35f5dfb886ea06eae1aa6e16d6
mohammed-elkrnshawy <mohammedelkrnshawy@gmail.com>
2019-03-01 12:11:41 +0200

    Reservation Class

---
+++ app/src/main/java/com/example/svago/ClassTransferActivity.java
@@ -0,0 +1,13 @@
+package com.example.svago;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClassTransferActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class_transfer);
+    }
+}

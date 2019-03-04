00ad22eba3ea29bcd220640201f8cc05eff6dabd
Mahmoud Hlal <mahmoudhlal741996@gmail.com>
2019-03-04 21:37:29 +0200

    view and pick date

---
+++ app/src/main/java/com/example/svago/Activity/PickDateActivity.java
@@ -0,0 +1,15 @@
+package com.example.svago.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.svago.R;
+
+public class PickDateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick_date);
+    }
+}

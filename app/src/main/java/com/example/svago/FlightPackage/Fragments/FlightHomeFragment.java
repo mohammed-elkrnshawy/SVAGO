9be8a2b9df3026f9e78a964155f6c350d5b5e6c3
mohammed-elkrnshawy <mohammedelkrnshawy@gmail.com>
2019-03-10 22:27:10 +0200

    Bind Application

--- app/src/main/java/com/example/svago/FlightPackage/Fragments/FlightHomeFragment.java
+++ app/src/main/java/com/example/svago/FlightPackage/Fragments/FlightHomeFragment.java
@@ -1,27 +1,44 @@
 package com.example.svago.FlightPackage.Fragments;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.svago.R;
 
 
+@SuppressLint("ValidFragment")
 public class FlightHomeFragment extends Fragment {
 
-    public FlightHomeFragment() {
-        // Required empty public constructor
+    private ImageView imgFlight;
+
+    public FlightHomeFragment(ImageView imgFlight) {
+        this.imgFlight=imgFlight;
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_flight_home, container, false);
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        imgFlight.setColorFilter(ContextCompat.getColor(getContext(), R.color.ColorPink), android.graphics.PorterDuff.Mode.SRC_IN);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        imgFlight.setColorFilter(ContextCompat.getColor(getContext(), R.color.ColorGray), android.graphics.PorterDuff.Mode.SRC_IN);
+    }
 }
--- app/src/main/java/com/example/svago/SharedPackage/Activity/MainHomeActivity.java
+++ app/src/main/java/com/example/svago/SharedPackage/Activity/MainHomeActivity.java
@@ -1,20 +1,48 @@
 package com.example.svago.SharedPackage.Activity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.example.svago.FlightPackage.Fragments.FlightHomeFragment;
 import com.example.svago.R;
 import com.example.svago.SharedPackage.Classes.Constant;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainHomeActivity extends AppCompatActivity {
+
+    @BindView(R.id.homeToolbar)
+    Toolbar toolbar;
+
+
+    private RelativeLayout relativeFlight,relativeHotel,relativeCar;
+    private ImageView imgFlight,imgHotel,imgCar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_home);
+        ButterKnife.bind(this);
+        initComponents();
+        setToolBar();
         getIntentData();
+    }
+
+    private void initComponents() {
+
     }
 
     private void getIntentData() {
@@ -31,7 +59,7 @@ public class MainHomeActivity extends AppCompatActivity {
         }
         else if (type.equals(Constant.FlightTag))
         {
-            setFragment(new FlightHomeFragment(),getString(R.string.flight));
+            setFragment(new FlightHomeFragment(imgFlight),getString(R.string.flight));
         }
         else if (type.equals(Constant.HotelTag))
         {
@@ -42,5 +70,17 @@ public class MainHomeActivity extends AppCompatActivity {
     private void setFragment(Fragment fragment, String Title) {
         getSupportFragmentManager().beginTransaction().replace(R.id.conainerHome,fragment).addToBackStack(Title)
                 .commitAllowingStateLoss();
+    }
+
+    private void setToolBar(){
+        setSupportActionBar(toolbar);
+        final LayoutInflater inflator = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflator.inflate(R.layout.bar_home, null);
+
+        imgFlight=view.findViewById(R.id.imgFlight);
+        imgHotel=view.findViewById(R.id.imgHotel);
+        imgCar=view.findViewById(R.id.imgCar);
+
+        toolbar.addView(view);
     }
 }
